80c0208bc05ff14804e17b9500d5b41c2ffdf2e1
elpinal <elpinal@users.noreply.github.com>
2016-07-28 11:30:13 +0900

    Add fuzzing

---
+++ fuzz.go
@@ -0,0 +1,13 @@
+package extract
+
+import "bytes"
+
+// +build gofuzz
+
+func Fuzz(data []byte) int {
+	b :=  bytes.NewBuffer(data)
+	if _, _, err := Extract(b); err != nil {
+		return 0
+	}
+	return 1
+}
